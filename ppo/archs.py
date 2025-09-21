369fc58b97a9a0527cd3141cc1ed66e48298fd41
RDLigeralde <rdligeralde@gmail.com>
2025-09-20 22:21:54 -0400

    add toggle to scan usage

--- ppo/archs.py
+++ ppo/archs.py
@@ -11,7 +11,8 @@ class LidarOdomBlender(BaseFeaturesExtractor):
         observation_space: Space,
         num_agents: int,
         num_odom_layers: int,
-        num_cline_layers: int = 0
+        num_cline_layers: int = 0,
+        use_convs: bool = True
     ):
         """
         Combines TinyLidarNet from 
@@ -21,8 +22,9 @@ class LidarOdomBlender(BaseFeaturesExtractor):
         self.num_beams = observation_space['scan'].shape[-1]
         self.n_cline_points = observation_space['cline_ctx'].shape[-2]
         self.num_odom_layers = num_odom_layers
-        features_dim = self._conv_outsize() * num_agents
-        super().__init__(observation_space, features_dim=features_dim)
+        self.use_convs = use_convs
+        self.embed_size = self._conv_outsize() * num_agents
+        super().__init__(observation_space, features_dim=self.embed_size)
 
         self.convs = nn.Sequential(
             nn.Conv1d(1, 24, kernel_size=10, stride=4),
@@ -35,42 +37,45 @@ class LidarOdomBlender(BaseFeaturesExtractor):
             nn.ReLU(),
             nn.Conv1d(64, 64, kernel_size=3, stride=1),
             nn.ReLU(),
-        )
+        ) if use_convs else None
 
         self.odom_mlp = nn.ModuleList()
         if num_odom_layers > 0:
-            self.odom_mlp.append(nn.Linear(5 * num_agents, features_dim)) # odom has 8 features
+            self.odom_mlp.append(nn.Linear(5 * num_agents, self.embed_size)) # odom has 8 features
             self.odom_mlp.append(nn.ReLU())
             for _ in range(num_odom_layers - 1):
-                self.odom_mlp.append(nn.Linear(features_dim, features_dim))
+                self.odom_mlp.append(nn.Linear(self.embed_size, self.embed_size))
                 self.odom_mlp.append(nn.ReLU())
 
         self.cline_mlp = nn.ModuleList()
         if num_cline_layers > 0:
-            self.cline_mlp.append(nn.Linear(self.n_cline_points * 2 * num_agents, features_dim))
+            self.cline_mlp.append(nn.Linear(self.n_cline_points * 2 * num_agents, self.embed_size))
             self.cline_mlp.append(nn.ReLU())
             for _ in range(num_cline_layers - 1):
-                self.cline_mlp.append(nn.Linear(features_dim, features_dim))
+                self.cline_mlp.append(nn.Linear(self.embed_size, self.embed_size))
                 self.cline_mlp.append(nn.ReLU())
 
     def forward(self, obs: Dict[str, torch.Tensor]) -> torch.Tensor:
         scan, odom, cline = obs["scan"], obs["odometry"], obs["cline_ctx"]
-        scan = scan.reshape(scan.shape[0], -1).unsqueeze(1) # (n_envs, 1, n_agents * num_beams)
+        scan = scan.reshape(scan.shape[0], -1).unsqueeze(1) # (n_envs, 1, num_beams)
         odom = odom.reshape(odom.shape[0], -1) # (n_envs, n_agents * odom_dim)
         cline = cline.reshape(cline.shape[0], -1) # (n_envs, n_agents * n_cline_points * 2)
 
-        scan = self.convs(scan)
-        scan = torch.flatten(scan, start_dim=1)
+        if self.use_convs:
+            x = self.convs(scan)
+            x = torch.flatten(x, start_dim=1)
+        else:
+            x = torch.zeros((odom.shape[0], self.embed_size), device=odom.device)
         if len(self.odom_mlp) > 0:
             for layer in self.odom_mlp:
                 odom = layer(odom)
-            scan = scan + odom
+            x = x + odom
         if len(self.cline_mlp) > 0:
             for layer in self.cline_mlp:
                 cline = layer(cline)
-            scan = scan + cline
+            x = x + cline
 
-        return scan
+        return x
 
     def _conv_outsize(self) -> int:
         """Gets output size of conv layers for num_beams"""
